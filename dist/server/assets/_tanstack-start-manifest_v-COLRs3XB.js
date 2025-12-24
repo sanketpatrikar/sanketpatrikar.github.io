4fff19ac59f04601bdf56a4a70a49edb292f6a41
Sanket Patrikar <sanketspatrikar@gmail.com>
2025-12-24 19:46:21 +0530

    remove & ignore dist

--- dist/server/assets/_tanstack-start-manifest_v-COLRs3XB.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "D:/code/ARCHIVE/site/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-Du1qTH4x.js"], "assets": [] }, "/": { "filePath": "D:/code/ARCHIVE/site/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-DqZ5KJaE.js"] } }, "clientEntry": "/assets/main-Du1qTH4x.js" });
-export {
-  tsrStartManifest
-};
